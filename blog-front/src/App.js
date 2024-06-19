afd3bf5ef6ae376e96e39b3443eb401ef69f8c06
Youssouf <youssouf.brahimazib@vinci-autoroutes.com>
2024-06-19 15:10:53 +0200

    initial blog front

---
+++ blog-front/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "@material-tailwind/react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <Button>Click me !</Button>
+    </div>
+  );
+}
+
+export default App;
